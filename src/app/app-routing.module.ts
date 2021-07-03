fb133bab7f9e4fbb097e86ec0c240a58b27320bb
Cre8ive-collins <wilsoncollins44@yahoo.com>
2021-07-03 10:33:10 +0100

    project updare

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EarningsComponent } from './earnings/earnings.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent} ,
+  {path: 'inventory', component: InventoryComponent} ,
+  {path: 'orders', component: OrdersComponent},
+  {path: 'earnings', component: EarningsComponent},
+  {path: 'settings', component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [DashboardComponent, InventoryComponent, OrdersComponent, EarningsComponent, SettingsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { OrdersComponent } from './orders/orders.component';
+import { EarningsComponent } from './earnings/earnings.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    routingComponent,
+    OrdersComponent,
+    EarningsComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
